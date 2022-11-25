4d4958ea9334a64a5981f6ddc753f3a92868664d
seemeest <44192374+seemeest@users.noreply.github.com>
2022-11-25 15:21:58 +0300

    32й4234

--- app/src/main/java/com/example/colemark/MainActivity.java
+++ app/src/main/java/com/example/colemark/MainActivity.java
@@ -3,15 +3,21 @@ package com.example.colemark;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.colemark.ui.main.MainFragment;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+      EditText name = findViewById(R.id.editTextTextPersonName2);
+    EditText  pass = findViewById(R.id.editTextNumberPassword);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())

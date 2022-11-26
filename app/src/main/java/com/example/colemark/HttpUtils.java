67d00deb63831c27e6a44cdf6dc56de09d78468e
minicx <levvoronov2006@gmail.com>
2022-11-26 18:34:51 +0300

    working back

---
+++ app/src/main/java/com/example/colemark/HttpUtils.java
@@ -0,0 +1,33 @@
+package com.example.colemark;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+
+public class HttpUtils {
+
+    private static String BASE_URL="http://api.nikitysikw.keenetic.pro:80/api/";//;
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void getByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(url, params, responseHandler);
+    }
+
+    public static void postByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(url, params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        System.out.println(BASE_URL + relativeUrl);
+        return BASE_URL + relativeUrl;
+    }
+}
--- app/src/main/java/com/example/colemark/MainActivity.java
+++ app/src/main/java/com/example/colemark/MainActivity.java
@@ -1,26 +1,74 @@
 package com.example.colemark;
 
+import com.loopj.android.http.*;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import com.example.colemark.ui.main.MainFragment;
+
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity {
+    static EditText email;
+    static EditText password;
+    static ImageButton loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email=findViewById(R.id.loginEmail);
+        password=findViewById(R.id.loginPassword);
+        loginButton=findViewById(R.id.loginButton);
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=MainActivity.email.getText().toString().trim();
+                String password=MainActivity.password.getText().toString().trim();
+                if (email.trim()=="" || password==""){
+                    // here error
+                    return;
+                }
+                RequestParams params = new RequestParams();
+                params.put("email",email);
+                params.put("password",password);
+
+                HttpUtils.post("users/login",params,new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        // checks
+
+                        System.out.println((new String(responseBody)).toString());
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                        // print error with network
+                        // System.out.println(error.toString()+"\n"+(new String(responseBody)).toString());
+                    }
+
+//
+                });
+
+                System.out.println("TEST");
+            }
+        });
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
         }
+
     }
 }

fc03f53c62686e47bbec0c0c4bbbf22d38de352e
seemeest <44192374+seemeest@users.noreply.github.com>
2022-11-27 08:00:31 +0300

    RegActiv Настроен

---
+++ app/src/main/java/com/example/colemark/ProFileActivity.java
@@ -0,0 +1,14 @@
+package com.example.colemark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProFileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pro_file);
+    }
+}

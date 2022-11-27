87d906f080cbf0c68b0b3063be173edd7cdd8d0f
minicx <levvoronov2006@gmail.com>
2022-11-27 10:43:07 +0300

    temp commit due test

--- app/src/main/java/com/example/colemark/ProfileActivity.java
+++ app/src/main/java/com/example/colemark/ProfileActivity.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.loopj.android.http.AsyncHttpResponseHandler;
@@ -13,6 +14,7 @@ import com.loopj.android.http.RequestParams;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -24,6 +26,13 @@ public class ProfileActivity extends AppCompatActivity {
     static TextView name;
     @SuppressLint("StaticFieldLeak")
     static TextView surName;
+
+
+    static ListView[] events=new ListView[3];
+    static ListView[] gifts=new ListView[3];
+    static ListView[] groups=new ListView[3];
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SharedPreferences mPrefs = getSharedPreferences("settings", 0);
@@ -42,8 +51,22 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pro_file);
         name=findViewById(R.id.profileProfileName);
         surName=findViewById(R.id.profileProfileSurname);
+
+        events[0]=findViewById(R.id.profile1List1);
+        events[1]=findViewById(R.id.profile1List2);
+        events[2]=findViewById(R.id.profile1List3);
+
+        gifts[0]=findViewById(R.id.profile2List1);
+        gifts[1]=findViewById(R.id.profile2List2);
+        gifts[2]=findViewById(R.id.profile2List3);
+
+        groups[0]=findViewById(R.id.profile3List1);
+        groups[1]=findViewById(R.id.profile3List2);
+        groups[2]=findViewById(R.id.profile3List3);
+
         // users
         HttpUtils.client.addHeader("Authorization","Bearer "+token);
+
         HttpUtils.get("users", new RequestParams(), new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
@@ -80,5 +103,41 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+
+        HttpUtils.get("events", new RequestParams(), new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                JSONObject response=new JSONObject();
+                if (statusCode==200){
+                    try {
+                        response=new JSONObject((new String(responseBody)));
+                    } catch (JSONException ignored) {
+
+                    }
+
+                    try {
+                        ProfileActivity.name.setText(response.getString("name"));
+                        ProfileActivity.surName.setText(response.getString("surname"));
+                    } catch (Exception ignored){
+
+                    }
+
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                System.out.println("ERROR");
+                if (statusCode==404){
+                    SharedPreferences mPrefs = getSharedPreferences("settings", 0);
+                    SharedPreferences.Editor mEditor = mPrefs.edit();
+                    mEditor.putString("token", "").apply();
+                    Intent intentMain = new Intent(ProfileActivity.this ,
+                            MainActivity.class);
+
+                    ProfileActivity.this.startActivity(intentMain);
+                }
+            }
+        });
     }
 }

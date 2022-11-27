aefc07d0b5ed7c08d33f7ee60f3623250a22e89b
seemeest <44192374+seemeest@users.noreply.github.com>
2022-11-27 12:06:55 +0300

    testing

--- app/src/main/java/com/example/colemark/ProfileActivity.java
+++ app/src/main/java/com/example/colemark/ProfileActivity.java
@@ -6,12 +6,17 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
@@ -55,15 +60,18 @@ public class ProfileActivity extends AppCompatActivity {
         events[0]=findViewById(R.id.profile1List1);
         events[1]=findViewById(R.id.profile1List2);
         events[2]=findViewById(R.id.profile1List3);
+        events[3]=findViewById(R.id.profile1List4);
 
         gifts[0]=findViewById(R.id.profile2List1);
         gifts[1]=findViewById(R.id.profile2List2);
         gifts[2]=findViewById(R.id.profile2List3);
+        gifts[3]=findViewById(R.id.profile2List4);
+
 
         groups[0]=findViewById(R.id.profile3List1);
         groups[1]=findViewById(R.id.profile3List2);
         groups[2]=findViewById(R.id.profile3List3);
-
+        groups[3]=findViewById(R.id.profile3List4);
         // users
         HttpUtils.client.addHeader("Authorization","Bearer "+token);
 
@@ -80,7 +88,13 @@ public class ProfileActivity extends AppCompatActivity {
 
                     try {
                         ProfileActivity.name.setText(response.getString("name"));
-                        ProfileActivity.surName.setText(response.getString("surname"));
+                        if (response.getString("surname")!=null){
+                            ProfileActivity.surName.setVisibility(View.INVISIBLE);
+                        } else{
+                            ProfileActivity.surName.setText(response.getString("surname"));
+                        }
+
+
                     } catch (Exception ignored){
 
                     }
@@ -107,20 +121,35 @@ public class ProfileActivity extends AppCompatActivity {
         HttpUtils.get("events", new RequestParams(), new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                JSONObject response=new JSONObject();
+                JSONArray response;
                 if (statusCode==200){
                     try {
-                        response=new JSONObject((new String(responseBody)));
+                        response=new JSONArray((new String(responseBody)));
+                        System.out.println(response);
                     } catch (JSONException ignored) {
+                        response=new JSONArray();
+                    }
+                    Button[] n={new Button(ProfileActivity.this)};
+
+                    for (int i=0;i!=response.length();i++){
+                        events[i].setAdapter(new ArrayAdapter<Button>(ProfileActivity.this, android.R.layout.simple_list_item_1,n));
+                    }
+                    for (int i=0;i!=3;i++){
+                        n[0].setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                System.out.println(1);
+                            }
+                        });
 
                     }
 
-                    try {
-                        ProfileActivity.name.setText(response.getString("name"));
-                        ProfileActivity.surName.setText(response.getString("surname"));
-                    } catch (Exception ignored){
-
-                    }
+//                    try {
+//                        ProfileActivity.name.setText(response.getString("name"));
+//                        ProfileActivity.surName.setText(response.getString("surname"));
+//                    } catch (Exception ignored){
+//
+//                    }
 
                 }
             }
